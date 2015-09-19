93bbdbdded3667ebba4847aa8c9d5a3031940d77
Punpikorn <sadanthem@gmail.com>
2015-09-20 05:11:11 +0700

    move the script to another file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-(function() {
-    'use strict';
-
-    var app = angular.module('application', []);//ชื่อโมดูล, โมดูลที่เกี่ยวข้อง(จะกล่าวถึงในภายหลัง)
-})(); // Best Practice For Javascript
+var app = angular.module('application', []);
+app.controller('main', function($scope) {
+  //Mock the tabs and task
+  $scope.tabs = [{name:"Default", tasks:[{name: 'Have Meeting'}], }, {name: 'Work', tasks:[{name: 'Do TSP Document'}]}];
+  $scope.addNewTask = function(tab) {
+    tab.tasks.push({name:$scope.taskName});
+    //clear the input text
+    $scope.listName = '';
+  };
+  $scope.addNewTab = function() {
+    $scope.tabs.push({name:$scope.tabName, task:[]});
+    //clear the input text
+    $scope.tabName = '';
+  };
+  $scope.deleteTask = function(task, tab) {
+    var index = tab.tasks.indexOf(task);
+    tab.tasks.splice(index, 1);
+  }
+  $scope.moveTo = function(otab, tab, task) {
+    $scope.deleteTask(task, tab);
+    otab.tasks.push(task);
+  }
+});
